9126b4af70e879d4e58767f4ec0ebaed239fed29
Jashawn <rogersjashawn20@gmail.com>
2022-11-03 10:14:55 -0700

    Added button toggle on Add btn & fixed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onAdd} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
 
 Header.defaultProps = {
-  title: "task tracker",
+  title: "Task Tracker",
 };
 
 Header.protoTypes = {
